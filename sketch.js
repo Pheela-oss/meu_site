ab5d69dfc4a03e687c134ed7e4e088e5f5165d9d
Pheela-oss <phelaniko20@gmail.com>
2026-06-17 20:21:10 -0300

    17 de jun 20:21

--- sketch.js
+++ sketch.js
@@ -1,57 +1,55 @@
-const loginButton = document.getElementById("loginButton")
-const nickname = document.getElementById("Nickname");
-const password = document.getElementById("Password");
-const deleteUser = document.getElementById("excluirConta")
-
-loginButton.addEventListener("click", function () {
-    const nickname_storage = localStorage.getItem("username");
-    const password_storage = localStorage.getItem("password");
-    
-    if (nickname.value == "" || password.value == ""){
-        alert("Nickname/Senha Vazia")
-    } else {
-        login(password_storage, nickname_storage);
-    }
-
-    console.log(nickname_storage);
-    console.log(password_storage)
-});
-
-function login(password_storage, nickname_storage){
-    const imagem = document.getElementById("emotion profile")
-    const letra = document.getElementById("h1fromLEM")
-
-    if ( password_storage === null || nickname_storage === null){
-        localStorage.setItem("username", nickname.value);
-        localStorage.setItem("password", password.value);
-        imagem.src = "assets/content/media/Bothered Kuromi.jpg"
-    } else {
-        if (password_storage === password.value && nickname_storage === nickname.value){
-            imagem.src = "assets/content/media/Happy Kuromi.jpg"
-            letra.textContent = "Login com sucesso!"
-            loginPopUp();
-        } else {
-            imagem.src = "assets/content/media/Scared Kuromi.jpg"
-            letra.textContent = "usuario/senha incorreto!"
-            loginPopUp();
-        }
-    }
-}
-
-function loginPopUp(){
-    const modal = document.querySelector(".loginErrorMessages")
-    modal.style.top = "20px"
-
-    setTimeout(function() {
-        modal.style.top = "-150px"
-    }, 1500)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, deleteUser } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js"
+          
+const firebaseConfig = {
+    apiKey: "AIzaSyBg35YqylCjeYa074djcTnkdmI-USfh31M",
+    authDomain: "portfolio-59957.firebaseapp.com",
+    projectId: "portfolio-59957",
+    storageBucket: "portfolio-59957.firebasestorage.app",
+    messagingSenderId: "684060252659",
+    appId: "1:684060252659:web:cc6ce770ba429f45709a86",
+    measurementId: "G-CGWM9PX4W8"
 };
 
-deleteUser.addEventListener("click", function() {
-    const imagem = document.getElementById("emotion profile");
-    const letra = document.getElementById("h1fromLEM")
-    localStorage.clear();
-    imagem.src = "assets/content/media/Bothered Kuromi.jpg"
-    letra.textContent = "Usuario Apagado :("
-    loginPopUp();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const loginButton = document.getElementById("Entrar");
+const nicknameInput = document.getElementById("Nickname");
+const passwordInput = document.getElementById("Password");
+const excluirConta = document.getElementById("excluirConta")
+const profMOTE = document.getElementById("emotion profile")
+const h1fromLEM = document.getElementById("h1fromLEM")
+const LoginerrorPOPUP = document.querySelector(".loginErrorMessages")
+
+// firebase & variavéis acima dessa linha
+
+loginButton.addEventListener("click", async function(){
+    let nickname = nicknameInput.value;
+    let password = passwordInput.value;
+    
+    try {
+        const resultado = await signInWithEmailAndPassword(auth, nickname, password);
+
+        console.log("deu certo!");
+    } catch(error) {
+        if (error.code === "auth/invalid-email"){
+            console.log("escreve direito analfabeto BURRO", error.message)
+        } else if (error.code === "auth/user-not-found"){
+            console.log("nao existe igual meu pai", error.message)
+            createUserWithEmailAndPassword(auth, nickname, password);
+        } else if (error.code === "auth/wrong-password"){
+            console.log("errou a senha bixo véi", error.message)
+        } else if (error.code === "auth/invalid-credential"){
+            console.log("ou nao existe ou tem algo errado", error.message);
+        } else {
+            console.log("deu merda, agora é só deus", error.message);
+        }
+    }
+
 });
+
+
+// estudando . . .
